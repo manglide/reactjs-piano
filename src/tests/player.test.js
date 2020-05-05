6371e6a78d3dd0f255924913ac5bb1fa7a70acfe
manglide <adeyemisalau@gmail.com>
2020-05-05 10:03:34 +0100

    'test the Generate Button'

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -20,5 +20,18 @@ it('renders the player component properly without errors', () => {
   act(() => {
     render(<Player />, container)
   });
-  
+})
+
+it('test the generate button', () => {
+  const onClick = jest.fn();
+  act(() => {
+    render(<Player generateTransition={onClick} />, container)
+  });
+  const button = container.querySelector("button");
+  act(() => {
+    button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+
+  // Simulate the Click on the Generate Element
+  expect(onClick).toHaveBeenCalledTimes(1);
 })
